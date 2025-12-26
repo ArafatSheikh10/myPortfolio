6f5f59b57ef91eec2415ce72ed751a731ba2916d
Arafat <placeholderause4590@gmail.com>
2025-12-26 22:18:29 +0600

    uplode done

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    // ==== Aos Start
+
+    AOS.init();
+
+    // ==== Aos End
+
+    //===== Sticky Menu-Bar Start
+
+    window.onscroll = function() {stickyNavbar()};
+
+    var navbar = document.querySelector(".nav-area");
+    
+    var stickyPoint = 100;
+    
+    function stickyNavbar() {
+      if (window.pageYOffset >= stickyPoint) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+    
+    //===== Sticky Menu-Bar End
+
+    //==== Nav Menu Slider Start
+
+        $('.nav_trigger').click(function(){
+            $('.sidebar, .overlay').addClass('slide_sidebar');
+        })
+
+        $('.nav_dismess, .overlay, .sidebar .nav-menu a').click(function(){
+            $('.sidebar, .overlay,').removeClass('slide_sidebar');
+        })
+
+    //==== Nav Menu Slider 
+    
+
+    // ==== Section Select Start
+    $('.nav-menu a[href^="#"],').on('click', function(e) {
+        e.preventDefault();
+        var target = $(this.hash);
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70 
+            }, 800);
+        }
+    });
+    // ==== Section Select End
+
+
+});
+
+
+
+
+    
